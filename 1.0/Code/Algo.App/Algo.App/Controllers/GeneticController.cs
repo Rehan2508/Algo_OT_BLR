313b5ba2ddd98e709181f9850ed5d47820c7a789
Nitin Mankar <Nitin.Mankar@Solera.com>
2022-12-27 17:31:01 +0530

    Genetic Algorithm

---
+++ 1.0/Code/Algo.App/Algo.App/Controllers/GeneticController.cs
@@ -0,0 +1,31 @@
+﻿using Algo.App.Data;
+using Algo.App.Dtos;
+using Algo.App.Services.GeneticService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algo.App.Controllers
+{
+	public class GeneticController : Controller
+	{
+        private readonly IGeneticService _geneticService;
+
+        public GeneticController(IGeneticService geneticService)
+        {
+            _geneticService = geneticService;
+        }
+        public IActionResult Index()
+        {
+            return View(new GetRouteDto());
+        }
+        public IActionResult GetRoute(GetRouteDto route)
+        {
+            var response = _geneticService.GetShortestPath(route);
+            return View(response);
+        }
+	}
+}
